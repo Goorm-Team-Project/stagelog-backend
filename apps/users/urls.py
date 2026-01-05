f8b5cca9217526c80e41eb3140a9c1c7be7a7818
woosupar <grademe12@gmail.com>
2026-01-05 16:01:26 +0900

    feat: kakao login

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import kakao_login, me
 
 urlpatterns = [
-    path('login', kakao_login, name='kakao_login'),
+    path('login/kakao', kakao_login, name='kakao_login'),
+    path('login/google', google_login, name='google_login'),
+    path('login/naver', naver_login, name='naver_login'),
+    path('signup', signup, name='signup'),
     path('me', me, name='user_me'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,6 @@
-import traceback
-import json
-import requests
+import traceback, json, sys, requests
+from .models import User
+from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import get_user_model
 from apps.common.utils import (
@@ -15,98 +15,122 @@ User = get_user_model()
 @csrf_exempt
 def kakao_login(request):
     """
-    POST /users/login/kakao/
-    Body: { "access_token": "카카오_액세스_토큰" }
-    기능: 카카오 토큰 유효성 검증 -> 유저 DB 조회/생성 -> JWT 발급
+    REST API KEY + 클라이언트 시크릿으로 카카오 auth 서버에 인가코드 요청. >> 프론트엔드 영역
+    인가코드 발급 후 액세스 토큰 요청. >> 여기서부터 백엔드 영역
+    발급된 액세스 토큰으로 사용자 정보 조회 등 실시.
+    https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api 참고
     """
-    if request.method != 'POST':
-        return common_response(success=False, message="POST 요청만 가능합니다.", status=405)
-
     try:
-        # 1. 프론트엔드(Client)에서 보낸 카카오 토큰 받기
         data = json.loads(request.body)
-        kakao_token = data.get('access_token')
+        code = data.get('code') #카카오에서 발급 인가 코드
+        if not code:
+            return common_response(success=False, message="인가 코드 에러.", status=400)
 
-        if not kakao_token:
-            return common_response(success=False, message="카카오 액세스 토큰이 필요합니다.", status=400)
+        access_token_req_url = "https://kauth.kakao.com/oauth/token" #POST 요청 사용
+        #KAKAO_ACCESS_TOKEN_CLIENT_SECRET = .env에서 클라이언트 시크릿 가져오기
+        KAKAO_REST_API_KEY = #.env 에서 앱에 할당된 REST API 가져오기
+        KAKAO_REDIRECT_URI = #.env 에서 앱에 등록한 응답 리다이렉트 주소
+        
+        access_token_req_data = {
+            "grant_type": "authorization_code",
+            "cliend_id" : KAKAO_REST_API_KEY,
+            "redirect_uri" : KAKAO_REDIRECT_URI,
+            "code" : code,
+            "client_secret" : KAKAO_ACCESS_TOKEN_CLIENT_SECRET
+        }
 
+        token_res = requests.post(access_token_req_url, data=access_token_req_data)
 
-        # 2. [Real] 카카오 서버에 "이 토큰 진짜니?" 물어보기
-        headers = {"Authorization": f"Bearer {kakao_token}"}
-        kakao_response = requests.get("https://kapi.kakao.com/v2/user/me", headers=headers)
+        if token_res.status_code != 200:
+            return common_response(success=False, message="카카오 액세스 토큰 발급 실패", status=400)
+        
+        """
+        액세스 토큰 발급 이후 사용자 정보 조회
+        """
 
-        # 3. 토큰이 유효하지 않으면 카카오가 에러 뱉음
-        if kakao_response.status_code != 200:
-            return common_response(success=False, message="유효하지 않은 카카오 토큰입니다.", status=401)
+        kakao_access_token = token_res.json().get('access_token')
 
-        # 4. 카카오 프로필 정보 추출
-        user_info = kakao_response.json()
-        # kakao_account = user_info.get('kakao_account', {})
-        # profile = kakao_account.get('profile', {})
+        user_info_url = "https://kapi.kakao.com/v2/user/me"
+        headers = {"Authorization": f"Bearer {kakao_access_token}"}
+        users_res = requests.get(user_info_url, headers=headers)
 
-        # 이메일은 필수 (카카오 개발자 센터에서 이메일 제공 동의 필수 설정 필요)
-        input_email = data.get('email')
-        input_nickname = data.get('nickname')
-        provider_id = str(user_info.get('id')) # 카카오 고유 ID
-        # nickname = profile.get('nickname', f'User{provider_id[:4]}')
+        if users_res.status_code != 200:
+            return common_response(success=False, message="사용자 정보 조회 실패", status=400)
 
-        if not input_email:
-            return common_response(success=False, message="이메일 정보가 없습니다.", status=400)
-        if not input_nickname:
-            return common_response(success=False, message="닉네임 정보가 없습니다.", status=400)
+        user_info = user_res.json()
+        provider_id = str(user_info.get('id'))
 
-        # 5. [DB 처리] 회원가입 or 로그인
-        try:
-            # 5-1. 이미 가입된 유저 찾기
-            user = User.objects.get(email=input_email)
-            message = "로그인 성공"
-            
-            # (선택) 닉네임이 바뀌었으면 업데이트
-            # if user.nickname != nickname:
-            #     user.nickname = nickname
-            #     user.save()
+        """
+        provider_id 이용해서 DB에 유저 정보 확인하기
+        """
+        
+        user = User.objects.filter(provider='kakao', provider_id=provider_id).first()
 
-        except User.DoesNotExist:
-            # 5-2. 없으면 새로 생성 (회원가입)
-            # models.py에 정의된 UserManager의 create_user 사용
-            user = User.objects.create_user(
-                email=input_email,
-                nickname=input_nickname,
-                provider='kakao',
-                provider_id=provider_id
-            )
-            message = "회원가입 및 로그인 성공"
-
-        # 6. [JWT 발급] 우리 서비스 전용 토큰 생성 (utils.py 사용)
-        access_token = create_access_token(user.id)
-        refresh_token = create_refresh_token(user.id)
-
-        return common_response(
-            success=True, 
-            message=message, 
-            data={
-                "token": {
-                    "access": access_token, 
-                    "refresh": refresh_token
+        if user:
+            jwt_access_token = create_access_token(user.id)
+            return common_response(success=True, message="f{user.nickname} 님! 환영합니다!", status=200)
+        
+        else:
+            register_payload = {
+                "provider" : "kakao",
+                "provider_id" : provider_id,
+                "email" : user_info.get('kakao_account', {}).get('email', ''),
+                "exp" : datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
+            }
+            register_token = jwt.encode(register_payload, SECRET_KEY, algorithm="HS256")
+            return common_response(
+                success=True,
+                message="회원가입이 필요합니다.",
+                data={
+                    "register_token": register_token,
+                    "email" : register_payload["email"]
                 },
-                "user": {
-                    "id": user.id,
-                    "email": user.email,
-                    "nickname": user.nickname,
-                    "level": user.level # models.py 기본값 확인
-                }
-            },
-            status=200
+                status=202
+            )
+    except Exception as e:
+        traceback.print_exc(file=sys.stdout)
+        return common_response(success=False, message="알 수 없는 오류", status=500)
+
+@csrf_exempt
+def signup(request):
+    try:
+        data = json.loads(request.body)
+        register_token = data.get('register_token')
+        input_nickname = data.get('nickname')
+        input_email = data.get('email')
+        input_provider = data.get('provider')
+
+        if not register_token:
+            return common_response(success=False, message="회원가입 JWT 토큰이 없습니다.", status=400)
+
+        try:
+            payload = jwt.decode(register_token, SECRET_KEY, algorithm="HS56"])
+        except jwt.ExpiredSignatureError:
+            return common_response(success=False, message="가입 시간이 만료되었습니다.", status=400)
+        except jwt.InvalidTokenError:
+            return common_response(succes=False, message="유효하지 않은 토큰입니다.", status=400)
+
+        provider_id = payload.get('provider_id')
+
+        if User.objects.filter(provider='kakao', provider_id=provider_id).exists():
+            return common_response(succes=False, message="이미 가입된 회원입니다.", status=400)
+        if User.objects.filter(email=email).exists():
+            return common_response(succes=False, message="이미 가입된 회원입니다.", status=400)
+        
+        user = User.objects.create_user(
+            email=input_email,
+            nickname=input_nickname,
+            provider=input_provider,
+            provider_id=provider_id
         )
 
-    except Exception as e:
-        # 실제 운영 시에는 로깅 필요
-        # print(f"Login Error: {e}")
-        print("======================== 에러 로그 =======================", flush=True)
-        traceback.print_exc()
-        print("======================== 에러 로그 =======================", flush=True)
+        access_token = create_access_token(user.id)
+        return common_response(success=Ture, message"회원가입이 완료되었습니다.", data={"acess_token": access_token})
 
-        return common_response(success=False, message="서버 내부 오류 발생", status=500)
+    except Exception as e:
+        traceback.print_exc(file=sys.stdout)
+        return common_response(succes=False, message="에러 발생", status=500)
+
 
 # 내 정보 조회 (토큰 검증 테스트용)
 @login_check
