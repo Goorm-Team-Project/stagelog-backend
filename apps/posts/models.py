4a003005f432902ecac7ea1a46095fe75c17dc29
danpro94 <22314521+danpro94@users.noreply.github.com>
2026-01-08 10:39:31 +0900

    fix(posts): models.py 게시글 이미지 필드 추가, 댓글 테이블 추가

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -33,6 +33,10 @@ class Post(models.Model):
     dislike_count = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
 
+    # 260107: 게시판 이미지 추가 기능 미팅에 따라 추가
+    # [ERD v3 최신 반영]
+    image_url = models.CharField(max_length=255, blank=True, null=True)
+
     class Meta:
         db_table = "posts"
         indexes = [
@@ -43,3 +47,37 @@ class Post(models.Model):
 
     def __str__(self):
         return f"Post({self.post_id}) {self.title}"
+
+# Comments 테이블
+class Comment(models.Model):
+    comment_id = models.BigAutoField(primary_key=True)
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        db_column="user_id",
+        related_name="comments",
+    )
+
+    post = models.ForeignKey(
+        "posts.Post",
+        on_delete=models.CASCADE,
+        db_column="post_id",
+        related_name="comments",
+    )
+
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "comments"
+        indexes = [
+            models.Index(fields=["post"]),
+            models.Index(fields=["user"]),
+            models.Index(fields=["created_at"]),
+        ]
+
+    def __str__(self):
+        return f"Comment({self.comment_id}) post_id={self.post_id}"
