55850deb1c9c013bbd74fa0d12fa1fecc7f30574
woosupar <grademe12@gmail.com>
2026-01-13 09:24:58 +0900

    fix: url slash fix

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -4,6 +4,6 @@ from posts import views as post_views
 
 urlpatterns = [
     path("", views.event_list, name="event_list"),
-    path('<int:event_id>', views.event_detail, name="event_detail"),
-    path('<int:event_id>/posts', post_views.event_posts_list, name="event_posts_list"),
+    path('/<int:event_id>', views.event_detail, name="event_detail"),
+    path('/<int:event_id>/posts', post_views.event_posts_list, name="event_posts_list"),
 ]
