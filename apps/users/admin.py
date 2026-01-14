8d52c2cd6c7d94ae8dc66a67d91c576153c67529
hanyong <woojoosocute@gmail.com>
2026-01-14 14:12:43 +0900

    fix: docker-compose.yml 바뀐부분만 마이그레이션, 관리자화면에서 유저 메뉴 보이게

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+# 가장 기본적인 등록 방법
+admin.site.register(User)
+
+# 만약 더 상세한 기능을 원하시면 아래처럼 사용해도 됩니다.
+# @admin.register(User)
+# class CustomUserAdmin(UserAdmin):
+#     model = User
+#     list_display = ['email', 'nickname', 'is_admin', 'is_staff']
