980e05621248bcba4df275673a922719b66dc3e1
hanyong <woojoosocute@gmail.com>
2026-01-14 11:43:50 +0900

    fix: 프론트 서버 없이 정상적인 장고화면 보기(임시방편)

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+import re
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +30,5 @@ urlpatterns = [
 
     # Events (events 라우팅)
     path('api/events/', include('events.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
