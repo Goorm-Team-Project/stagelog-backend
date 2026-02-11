7979f774933b1dfee2a4a635f1435ce4f18aee14
woosupar <grademe12@gmail.com>
2026-02-11 15:51:44 +0900

    fix: ip 블랙리스트 관련 버그 수정

--- apps/common/utils.py
+++ apps/common/utils.py
@@ -131,7 +131,7 @@ def get_optional_user_id(request):
 
     return user_id, None
 
-def get_client_ip(self, request):
+def get_client_ip(request): 
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
         ip = x_forwarded_for.split(',')[0].strip()
